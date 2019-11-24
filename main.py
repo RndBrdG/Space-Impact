47f68b5fca12906cda1d0281da417bbb01ceaaf2
Tiago <tiago@Earth.home>
2019-11-24 21:37:13 +0000

    Initial remake.

--- main.py
+++
@@ -1,5 +0,0 @@
-from Game import Game
-
-if __name__ == "__main__":
-    theApp = Game()
-    theApp.game_loop()
---
+++ space-impact/__main__.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+      # execute only if run as a script
+      print("Space Impact's remake.")
